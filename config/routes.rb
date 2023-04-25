e19a81c377969b9fe6075423a66d1c3ed8aa12ce
kzm0620 <kzm.staygold@gmail.com>
2023-04-25 16:49:14 +0900

    AjaxAppの準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'posts', to: 'posts#index'
-  get 'posts/new', to: 'posts#new'
+  root 'posts', to: 'posts#index'
+  
   post 'posts', to: 'posts#create'
 end
